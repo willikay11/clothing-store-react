a9c149ffb099d3cde40fd64eccfe9c0256fcd7a6
William Kamau <william.kamuyu@gapstack.com>
2022-10-23 10:47:03 +0300

    Added router

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import {Link, Outlet} from 'react-router-dom'
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+      <>
+          <div className="navigation">
+              <Link className="logo-container" to="/">
+                  <CrownLogo />
+              </Link>
+              <div className="nav-links-container">
+                  <Link class="nav-link" to="/shop">
+                      Shop
+
+                  </Link>
+              </div>
+          </div>
+          <Outlet />
+      </>
+  )
+}
+
+export default Navigation;
